d37d5d20f3dbe23a8cbda220d31f100f26eb6be6
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-11-26 19:07:05 +0000

    Historico de transacoes finished

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,7 +4,7 @@ import { GameOutcomes } from './GameOutcomes'
 export const Game = ({selectedOutcomes, game, outcomeClick}) => {
     
     return (
-    <div className='border-2 bg-gray-50 rounded-md border-dotted border-green-900 flex items-center '>
+    <div className='border-2 bg-gray-50 rounded-md border-dotted border-green-900 flex items-center'>
             <div className='flex flex-col justify-center m-4  min-w-[200px]'>
                 <p className='text-xl'><b>{game.home} - {game.away}</b></p>
                 <p className=' text-sm text-gray-400'>{game.date}</p>
--- src/components/HistoricoTransacoes.jsx
+++ src/components/HistoricoTransacoes.jsx
@@ -21,19 +21,71 @@ export const HistoricoTransacoes = ({isOnHistoricoTransacoes}) => {
         operacao: "+ 3 €",
         saldoDepois: 3,
 
+    },{
+        data: "18/11/2021",
+        descricao: "Ganho de aposta",
+        operacao: "+ 13,29 €",
+        saldoDepois: 13.29,
+    },{
+        data: "12/11/2021",
+        descricao: "Aposta",
+        operacao: "- 3 €",
+        saldoDepois: 0,
+
+    },{
+        data: "12/11/2021",
+        descricao: "Depósito",
+        operacao: "+ 3 €",
+        saldoDepois: 3,
+
+    },{
+        data: "18/11/2021",
+        descricao: "Ganho de aposta",
+        operacao: "+ 13,29 €",
+        saldoDepois: 13.29,
+    },{
+        data: "12/11/2021",
+        descricao: "Aposta",
+        operacao: "- 3 €",
+        saldoDepois: 0,
+
+    },{
+        data: "12/11/2021",
+        descricao: "Depósito",
+        operacao: "+ 3 €",
+        saldoDepois: 3,
+
+    },{
+        data: "18/11/2021",
+        descricao: "Ganho de aposta",
+        operacao: "+ 13,29 €",
+        saldoDepois: 13.29,
+    },{
+        data: "12/11/2021",
+        descricao: "Aposta",
+        operacao: "- 3 €",
+        saldoDepois: 0,
+
+    },{
+        data: "12/11/2021",
+        descricao: "Depósito",
+        operacao: "+ 3 €",
+        saldoDepois: 3,
+
     }]
 
     return (
     <div className={`${!isOnHistoricoTransacoes ? "active" : ""} gap-4 items-center show w-[1280px] h-[700px] bg-white border-dotted  border-[2px] border-green-900  flex flex-col shrink rounded-3xl p-4`}>
         <p className='text-4xl mt-4'>Histórico de Transações</p>
         <p className='text-2xl mt-4'>Saldo: {} €</p>
-        <table class="table-auto w-[1000px]">
-        <thead className="border-t-2 border-b-2 border-green-700">
+        <div className='w-[1000px] no-scrollbar overflow-y-auto'>
+        <table class="w-full">
+        <thead className="bg-white sticky top-0 h-[50px] text-xl">
             <tr>
-                <th className='text-left'>Data</th>
+                <th className='text-center ml-2'>Data</th>
                 <th className='text-left'>Descrição</th>
-                <th>Operação</th>
-                <th>Saldo após movimento</th>
+                <th className='text-left'>Operação</th>
+                <th className='text-center'>Saldo após movimento</th>
             </tr>
         </thead>
         <tbody>
@@ -41,16 +93,20 @@ export const HistoricoTransacoes = ({isOnHistoricoTransacoes}) => {
             transacoes.map((transacao) => {
                 return(
                     <tr>
-                        <td className='text-left'>{transacao.data}</td>
-                        <td className='text-left'>{transacao.descricao}</td>
-                        <td className='text-left'>{transacao.operacao}</td>
-                        <td className=" text-center">{transacao.saldoDepois}</td>
+                        <td className='text-center text-xl h-[50px]'>{transacao.data}</td>
+                        <td className='text-left text-xl h-[50px]'>{transacao.descricao}</td>
+                        <td className='text-left text-xl h-[50px]'>{transacao.operacao}</td>
+                        <td className='text-center text-xl h-[50px]'>{transacao.saldoDepois}</td>
                     </tr>
                 );
             })
         }
         </tbody>
         </table>
+        </div>
+        <hr className='w-[1000px] border-[1.5px] border-green-700 absolute top-[150px]'/>
+        <hr className='w-[1000px] border-[1.5px] border-green-700 absolute top-[200px]'/>
+        <hr className='w-[1000px] border-[1.5px] border-green-700 absolute bottom-[15px]'/>
     </div>
     )
 }
