937e4342ba7c87a8a16e6d0ef80553e2c8fc5db0
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-12-02 23:21:11 +0000

    Ligacao ao backend deposito

--- src/components/Deposito.jsx
+++ src/components/Deposito.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react';
+import {userDetailsContext} from "./UserDetailsProvider";
 
 export const Deposito = ({cancelAction}) => {
+    const [userDetails, setUserDetails] = useContext(userDetailsContext);
     const [inputValue,setInputValue] = useState("");
 
     const handleInputChange = (e) => {
@@ -8,7 +10,18 @@ export const Deposito = ({cancelAction}) => {
     }
 
     const handleDeposito = () => {
-
+        fetch("http://localhost:8080/apostador/depositar", {
+            method: "post",
+            body: JSON.stringify({
+                email: userDetails.email,
+                valor: inputValue,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        }).then((response) => response.json()).then(result => {
+            console.log(result);
+        });
     }
 
     return (
@@ -19,7 +32,7 @@ export const Deposito = ({cancelAction}) => {
         </div>
         <div className='flex gap-10'>
             <button onClick={() => {setInputValue(""); cancelAction();}} className=' border-orange-500 border-2 py-2 w-[140px] font-semibold text-orange-500 rounded-xl'>Cancelar</button>
-            <button onClick={() => setInputValue("siu")} className="bg-orange-500 text-white rounded-xl w-[140px] font-semibold">OK</button>
+            <button onClick={() => handleDeposito()} className="bg-orange-500 text-white rounded-xl w-[140px] font-semibold">OK</button>
         </div>
     </div>
     )
